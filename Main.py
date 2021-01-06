b2a03301bf84b18359a9aa37bb63365a804436d3
Michael <maestromike5@gmail.com>
2021-01-06 02:44:10 -0500

    finished implemented program logic and wrote print_friends helper function

--- Main.py
+++ Main.py
@@ -4,13 +4,24 @@ This file will act as the main logic for the program
 Author: VoxelVortex (Michael)
 VRCatFriendList > Main.py
 """
-import vrcpy, VRChatAdapter
+import vrcpy
+import VRChatAdapter
 
 
 def main():
     client = VRChatAdapter.login('login_info.json')
 
+    friends = VRChatAdapter.fetch_online_friends(client)
+
     VRChatAdapter.logout(client)
+
+    print_friends(friends)
+
+
+def print_friends(friends):
+    for friend in friends["friends"]:
+        print("{0}\n\tStatus: {1}\n\tPlatform: {2}"
+              .format(friend["displayName"], friend["status"], friend["last_platform"]))
 
 
 if __name__ == '__main__':
